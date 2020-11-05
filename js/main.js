0c27d9bf7325091d28290c31296b969158b06552
Kole Morgan <kmorgan0057@gmail.com>
2020-11-05 16:52:40 -0600

    JS-HTML Exercises

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
+"use strict";
+
 console.log("Hello from the JS console");
+alert("Welcome to my website!");
+
+var color = prompt("What is your favorite color?");
+alert("Great! "+ color + " is my favorite color too!");
