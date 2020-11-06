4af766d1bf910c155fd34286c5006a09cc90966e
Kole Morgan <kmorgan0057@gmail.com>
2020-11-06 11:52:16 -0600

    Function exercise edits

--- js/functions.js
+++ js/functions.js
@@ -19,6 +19,7 @@ function sayHello(name){
  * console.log 'helloMessage' to check your work
  */
 var helloMessage = sayHello("Kole");
+console.log(helloMessage);
 /**
  * TODO:
  * Store your name as a string in a variable named 'myName', and pass that
@@ -26,6 +27,7 @@ var helloMessage = sayHello("Kole");
  * console.
  */
 var myName = "Kole";
+console.log(sayHello(myName));
 // Don't modify the following line, it generates a random number between 1 and 3
 // and stores it in a variable named random
 var random = Math.floor((Math.random() * 3) + 1);
@@ -53,6 +55,8 @@ function isTwo(number){
     }else{return false}
 
 };
+console.log(isTwo(random));
+console.log(random);
 /**
  * TODO:
  * Create a function named 'calculateTip' to calculate a tip on a bill at a
