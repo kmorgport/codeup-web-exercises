988ec1cb803587804fbdacff3370889fe807892d
Kole Morgan <kmorgan0057@gmail.com>
2020-11-08 17:35:35 -0600

    Break and Continue

---
+++ js/break_and_continue.js
@@ -0,0 +1,13 @@
+function skipNumChoice(){
+    var begin = prompt("Please pick an odd number between 1 and 50.");
+    var checkNum = isNaN(begin);
+    if(check)
+};
+
+function doubleByTwo(){
+    var i = 2;
+    do{
+        console.log(i);
+        i*=2;
+    } while (i <=65536)
+};
