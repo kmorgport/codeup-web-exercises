73023b998ebe774955184e97690b24a27fcf34d6
Kole Morgan <kmorgan0057@gmail.com>
2020-11-08 18:04:59 -0600

    finished ice cream function

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -9,18 +9,18 @@ function doubleByTwo(){
 
 function iceCreamCones(){
     var allCones = Math.floor(Math.random() * 50) + 50;
-    if(allCones==0){
-        console.log("Yay! I sold them all!")
-    }else {
         do {
             var customer = Math.floor(Math.random() * 5) + 1;
             console.log(customer + " cones sold...");
-            allCones -= customer;
-            if (customer > allCones) {
+            if (allCones-customer < 0 ) {
                 console.log("Cannot sell you " + customer + " ice cream cones. I only have " + allCones + ".");
                 continue;
 
             }
+            allCones -= customer;
 
-        } while (allCones >0);
-    }};
+        } while (allCones>0);
+        if(allCones==0) {
+            console.log("Yay! I sold them all!")
+        }}
+iceCreamCones();
