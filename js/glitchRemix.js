fb1ace41f2c6247f4c90876401d07e39c6727346
Kole Morgan <kmorgan0057@gmail.com>
2020-11-11 16:15:33 -0600

    Glitch Remix Practice

---
+++ js/glitchRemix.js
@@ -0,0 +1,14 @@
+var onMarsRightNow = false;
+console.log(onMarsRightNow);
+
+var fruits = ["mango", "banana", "guava", "kiwi", "strawberry"]
+console.log(fruits);
+
+var vegetables = ["eggplant", "broccoli", "carrot", "cauliflower", "zucchini"];
+console.log(vegetables);
+
+var numbers = [1,2,3,4,5,6,7,8,9,10];
+console.log(numbers);
+
+console.log(fruits.push("tomato"));
+console.log(vegetables.push("tomato"));
