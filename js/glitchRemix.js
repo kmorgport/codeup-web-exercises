4238257ca90f6e417a5a8f54cec7b3a132682704
Kole Morgan <kmorgan0057@gmail.com>
2020-11-11 16:28:57 -0600

    Glitch Remix exercises

--- js/glitchRemix.js
+++ js/glitchRemix.js
@@ -1,14 +1,30 @@
+//1
 var onMarsRightNow = false;
 console.log(onMarsRightNow);
-
+//2
 var fruits = ["mango", "banana", "guava", "kiwi", "strawberry"]
 console.log(fruits);
-
+//3
 var vegetables = ["eggplant", "broccoli", "carrot", "cauliflower", "zucchini"];
 console.log(vegetables);
-
+//4
 var numbers = [1,2,3,4,5,6,7,8,9,10];
 console.log(numbers);
-
-console.log(fruits.push("tomato"));
-console.log(vegetables.push("tomato"));
+//5
+fruits.push("tomato");
+console.log(fruits[5]);
+//6
+vegetables.push("tomato");
+console.log(vegetables[5]);
+//7
+numbers.reverse();
+console.log(numbers);
+//8
+vegetables.sort();
+console.log(vegetables);
+//9
+fruits.reverse();
+console.log(fruits);
+//10
+var fruitsAndVeggies = fruits.concat(vegetables);
+console.log(fruitsAndVeggies);
