8adb755d48fd9c2f7b607cbe6b45eb5f143198a4
Kole Morgan <kmorgan0057@gmail.com>
2020-11-13 17:06:38 -0600

    Array work

--- js/arrayPractice.js
+++ js/arrayPractice.js
@@ -2,3 +2,18 @@ let foods = ["kimchi", "pizza", "calzones", "bulgogi", "sushi", "dorowat", "keba
 for(let i=0; i<foods.length; i++){
     console.log("I like eating " +foods[i]+".")
 };
+
+function sentenceBuilder(){
+    let foods = ["kimchi", "pizza", "calzones", "bulgogi", "sushi", "dorowat", "kebab"];
+    let sentBegin = "I like to eat "
+    for(let i=0; i<foods.length; i++){
+        if(i==foods.length-1){
+            sentBegin += "and "+foods[i]+".";
+        }else{
+            sentBegin += foods[i]+", ";
+        }
+    }
+    console.log(sentBegin);
+}
+
+sentenceBuilder();
