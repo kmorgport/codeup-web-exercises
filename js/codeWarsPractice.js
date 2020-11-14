a8d91b61c9315edc32d75292af23cd135cac5d85
Kole Morgan <kmorgan0057@gmail.com>
2020-11-14 13:28:11 -0600

    codewars practice

--- js/codeWarsPractice.js
+++ js/codeWarsPractice.js
@@ -28,3 +28,14 @@ function addBinary(a,b){
    var res = a+b;
    return parseInt(res,10).toString(2);
 }
+
+function validatePIN(pin) {
+    if(pin.match(/[^0-9]/)){
+        return false
+    }else if(pin.length!=4||pin.length!=6){
+        return false
+    }else{
+        return true
+    }
+
+}
