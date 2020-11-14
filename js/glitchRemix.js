7822e7cf2fc910956a61172399cbe449f537ff76
Kole Morgan <kmorgan0057@gmail.com>
2020-11-14 17:11:53 -0600

    Glitch Remix

--- js/glitchRemix.js
+++ js/glitchRemix.js
@@ -1,34 +1,34 @@
-//1
-var onMarsRightNow = false;
-console.log(onMarsRightNow);
-//2
-var fruits = ["mango", "banana", "guava", "kiwi", "strawberry"]
-console.log(fruits);
-//3
-var vegetables = ["eggplant", "broccoli", "carrot", "cauliflower", "zucchini"];
-console.log(vegetables);
-//4
-var numbers = [1,2,3,4,5,6,7,8,9,10];
-console.log(numbers);
-//5
-fruits.push("tomato");
-console.log(fruits[5]);
-//6
-vegetables.push("tomato");
-console.log(vegetables[5]);
-//7
-numbers.reverse();
-console.log(numbers);
-//8
-vegetables.sort();
-console.log(vegetables);
-//9
-fruits.reverse();
-console.log(fruits);
-//10
-var fruitsAndVeggies = fruits.concat(vegetables);
-console.log(fruitsAndVeggies);
-
+// //1
+// var onMarsRightNow = false;
+// console.log(onMarsRightNow);
+// //2
+// var fruits = ["mango", "banana", "guava", "kiwi", "strawberry"]
+// console.log(fruits);
+// //3
+// var vegetables = ["eggplant", "broccoli", "carrot", "cauliflower", "zucchini"];
+// console.log(vegetables);
+// //4
+// var numbers = [1,2,3,4,5,6,7,8,9,10];
+// console.log(numbers);
+// //5
+// fruits.push("tomato");
+// console.log(fruits[5]);
+// //6
+// vegetables.push("tomato");
+// console.log(vegetables[5]);
+// //7
+// numbers.reverse();
+// console.log(numbers);
+// //8
+// vegetables.sort();
+// console.log(vegetables);
+// //9
+// fruits.reverse();
+// console.log(fruits);
+// //10
+// var fruitsAndVeggies = fruits.concat(vegetables);
+// console.log(fruitsAndVeggies);
+//
 function randomPositiveEvenNumber(){
     var randomNumber = Math.ceil(Math.random()*100)+10;
     if(randomNumber % 2 !== 0){
@@ -64,51 +64,64 @@ var positiveEvenNumber =  randomPositiveEvenNumber();
 var positiveOddNumber = randomPositiveOddNumber();
 var negativeEvenNumber = randomNegativeEvenNumber();
 var negativeOddNumber = randomNegativeOddNumber();
+//
+// //11
+// function addOne(num){
+//     return num + 1;
+// }
+// console.log(positiveEvenNumber);
+// console.log(addOne(positiveEvenNumber));
+// console.log(negativeOddNumber);
+// console.log(addOne(negativeOddNumber));
+//
+// //12
+// function isPositive(num){
+//     return num>0;
+// }
+// console.log(isPositive(0.25));
+// console.log(isPositive(-.5));
+// console.log(positiveOddNumber);
+// console.log(isPositive(positiveOddNumber));
+// console.log(negativeEvenNumber);
+// console.log(isPositive(negativeEvenNumber));
+//
+// //13
+// function isNegative(num){
+//     return num<0;
+// }
+// console.log(positiveOddNumber);
+// console.log(positiveEvenNumber);
+// console.log(negativeEvenNumber);
+// console.log(negativeOddNumber);
+// console.log(isNegative(positiveOddNumber));
+// console.log(isNegative(positiveEvenNumber));
+// console.log(isNegative(negativeEvenNumber));
+// console.log(isNegative(negativeOddNumber));
+//
+// //14
+// function isOdd(num){
+//     return num%2 != 0;
+// }
+// console.log(positiveOddNumber);
+// console.log(positiveEvenNumber);
+// console.log(negativeEvenNumber);
+// console.log(negativeOddNumber);
+// console.log(isOdd(positiveOddNumber));
+// console.log(isOdd(positiveEvenNumber));
+// console.log(isOdd(negativeOddNumber));
+// console.log(isOdd(negativeEvenNumber));
+//
+// console.log("Repot testing");
 
-//11
-function addOne(num){
-    return num + 1;
-}
-console.log(positiveEvenNumber);
-console.log(addOne(positiveEvenNumber));
-console.log(negativeOddNumber);
-console.log(addOne(negativeOddNumber));
-
-//12
-function isPositive(num){
-    return num>0;
-}
-console.log(isPositive(0.25));
-console.log(isPositive(-.5));
-console.log(positiveOddNumber);
-console.log(isPositive(positiveOddNumber));
-console.log(negativeEvenNumber);
-console.log(isPositive(negativeEvenNumber));
-
-//13
-function isNegative(num){
-    return num<0;
-}
-console.log(positiveOddNumber);
-console.log(positiveEvenNumber);
-console.log(negativeEvenNumber);
-console.log(negativeOddNumber);
-console.log(isNegative(positiveOddNumber));
-console.log(isNegative(positiveEvenNumber));
-console.log(isNegative(negativeEvenNumber));
-console.log(isNegative(negativeOddNumber));
-
-//14
-function isOdd(num){
-    return num%2 != 0;
-}
-console.log(positiveOddNumber);
-console.log(positiveEvenNumber);
-console.log(negativeEvenNumber);
-console.log(negativeOddNumber);
-console.log(isOdd(positiveOddNumber));
-console.log(isOdd(positiveEvenNumber));
-console.log(isOdd(negativeOddNumber));
-console.log(isOdd(negativeEvenNumber));
-
-console.log("Repot testing");
+// //15
+// function isEven(num){
+//     return num%2 == 0;
+// }
+// console.log(positiveOddNumber);
+// console.log(isEven(positiveOddNumber))
+// console.log(positiveEvenNumber);
+// console.log(isEven(positiveEvenNumber))
+// console.log(negativeOddNumber);
+// console.log(isEven(negativeOddNumber));
+// console.log(negativeEvenNumber);
+// console.log(isEven(negativeEvenNumber));
