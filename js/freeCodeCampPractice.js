169297da04ab211cb3248b1a9dc0da388a231234
Kole Morgan <kmorgan0057@gmail.com>
2020-11-24 16:42:22 -0600

    CSS edits and JS practice

--- js/freeCodeCampPractice.js
+++ js/freeCodeCampPractice.js
@@ -21,19 +21,43 @@ var collection = {
 
 // Only change code below this line
 //create a function that updates nested objects
-function updateRecords(object, id, prop, value) {
-    //if the property enters is NOT 'tracks' AND the value is not an empty string
-    if(prop !== 'tracks' && value !== ''){
-        //update value to the objects property
-        object[id][prop] = value;
-    }else if(prop === 'tracks' && !object[id].hasOwnProperty('tracks')){
-        object[id][prop]=[value];
-    }else if(prop === 'tracks'&& value !==''){
-        object[id][prop].push(value);
-    }else if(value === ''){
-        delete object[id][prop]
-    }
-    return object;
-}
+// function updateRecords(object, id, prop, value) {
+//     //if the property enters is NOT 'tracks' AND the value is not an empty string
+//     if(prop !== 'tracks' && value !== ''){
+//         //update value to the objects property
+//         object[id][prop] = value;
+//     }else if(prop === 'tracks' && !object[id].hasOwnProperty('tracks')){
+//         object[id][prop]=[value];
+//     }else if(prop === 'tracks'&& value !==''){
+//         object[id][prop].push(value);
+//     }else if(value === ''){
+//         delete object[id][prop]
+//     }
+//     return object;
+// }
+//
+// updateRecords(collection, 5439, 'artist', 'ABBA');
 
-updateRecords(collection, 5439, 'artist', 'ABBA');
+// var myArr = [ 2,3,4,5,6]
+// var total = 0;
+// for(var i=0; i<myArr.length; i++){
+//     total += myArr[i];
+// }
+//
+// //use nested loops to multiply all elements of nested arrays
+// function multiplyAll(arr){
+//     var product = 0;
+//     for(var i =0; i<arr.length; i++){
+//         for(var j=0; j<arr[i].length;j++){
+//             product *=arr[i][j]
+//         }
+//     }
+// }
+
+// //push only 10 to array and stop loop when i= 11
+// var myArray = [];
+// var i = 10;
+// do{
+//     myArray.push(i);
+//     i++
+// }while(i<=10)
