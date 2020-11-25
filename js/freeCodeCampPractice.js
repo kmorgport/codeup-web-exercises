fb772e73da25ccff33a5e7df668c7c82d2772862
Kole Morgan <kmorgan0057@gmail.com>
2020-11-25 13:32:52 -0600

    JS practice

--- js/freeCodeCampPractice.js
+++ js/freeCodeCampPractice.js
@@ -130,3 +130,33 @@ var contacts = [
 
 //parseInt can also be used to parse non-decimal numbers (binary, hex, etc)
 // parseInt("11", 2)
+// function checkSign(num){
+//     return(num>0)?"positive"
+//         :(num<0)?"negative"
+//             :"zero";
+// }
+// //multiple ternaries
+//Using recursion to create countdown
+// function countup(n){
+//     if(n<1){
+//         return [];
+//     }else{
+//         //works down the tree (creates 5,4,3,2,1)
+//         const countArray = countup(n-1);
+//         //pushes back up in reverse order (1,2,3,4,5)
+//         countArray.push(n);
+//         return countArray;
+//     }
+// }
+// console.log(countup(5))//[1,2,3,4,5]
+function countDown(n){
+    if(n<1){
+        return [];
+    }else{
+        //works down tree (5,4,3,2,1)
+        const countArray = countDown(n-1);
+        //works back up tree(1,2,3,4,5) however, is unshifting to beginning of stack so numbers are pushed right (final array [5,4,3,2,1]
+        countArray.unshift(n);
+        return countArray;
+    }
+}
