e92e33af6ad13561aff5c68dd6a6c1ff897cbf13
Kole Morgan <kmorgan0057@gmail.com>
2020-11-26 14:25:29 -0600

    more fun with arrow functions

--- js/ES6Practice.js
+++ js/ES6Practice.js
@@ -16,5 +16,14 @@
 //     return new Date();
 // };
 
-//when the function only has a return value, you can forgo using the curly brackets entirely
-const magic = ()=> new Date();
+// //when the function only has a return value, you can forgo using the curly brackets entirely
+// const magic = ()=> new Date();
+
+
+// //arrow functions with parameters
+// var myConcat = function(arr1, arr2) {
+//     "use strict";
+//     return arr1.concat(arr2);
+// };
+//if there is only parameter you do not need to use the parenthesis
+const myConcat = (arr1, arr2) => arr1.concat(arr2);
