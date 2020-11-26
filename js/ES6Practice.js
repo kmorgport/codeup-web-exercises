c34ebe161cab8cb1a2d0764041573c7ae06926e5
Kole Morgan <kmorgan0057@gmail.com>
2020-11-26 14:19:16 -0600

    Arrow Function practice

--- js/ES6Practice.js
+++ js/ES6Practice.js
@@ -1,10 +1,20 @@
-//constants cant be altered
-const s = [5, 7, 2];
-function editInPlace() {
-    'use strict';
-    //however values inside an array which is assigned to a constant are mutable
-    s[0] = 2;
-    s[1] = 5;
-    s[2] = 7
+// //constants cant be altered
+// const s = [5, 7, 2];
+// function editInPlace() {
+//     'use strict';
+//     //however values inside an array which is assigned to a constant are mutable
+//     s[0] = 2;
+//     s[1] = 5;
+//     s[2] = 7
+//
+// }
 
-}
+//Using Arrow Functions
+//Turn this function into an arrow function
+// var magic = function() {
+//     "use strict";
+//     return new Date();
+// };
+
+//when the function only has a return value, you can forgo using the curly brackets entirely
+const magic = ()=> new Date();
