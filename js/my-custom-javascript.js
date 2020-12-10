b24df7e3e1cfd499a80c1eb3aaaad0a3d307d017
Kole Morgan <kmorgan0057@gmail.com>
2020-12-09 18:40:44 -0600

    Jquery Seletors

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -1,4 +1,9 @@
-"use strict"
-$(document).ready(function(){
-    alert('The DOM has finished loading!');
-})
+ "use strict"
+// $(document).ready(function(){
+//     alert('The DOM has finished loading!');
+// })
+// var cont = $('#ruby').html();
+// alert(cont);
+// $('.codeup').css('border','1px solid red');
+
+$('li').css('font-size','20px')
