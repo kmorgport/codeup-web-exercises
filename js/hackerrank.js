ec4018f08ede418d8184c7408719556e5672a341
Kole Morgan <kmorgan0057@gmail.com>
2020-12-11 16:45:59 -0600

    Hacker Rank

--- js/hackerrank.js
+++ js/hackerrank.js
@@ -5,3 +5,11 @@ function bignumSum(arr){
     console.log(sum)
 }
 bignumSum([1000000001,1000000002,1000000003,1000000004,1000000005,])
+
+function diagDiff(arr){
+    const diag = arr[0][0]+arr[1][1]+arr[2][2];
+    const revdiag = arr[0][2]+arr[1][1]+arr[2][0];
+    const sum = diag - revdiag;
+    console.log(Math.abs(sum))
+}
+diagDiff([[11,2,4],[4,5,6],[10,8,-12]])
