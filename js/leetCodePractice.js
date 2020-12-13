8aa2490ef518184abaf21cb93b289aa05245d720
Kole Morgan <kmorgan0057@gmail.com>
2020-12-13 17:55:33 -0600

    Leetcode Practice

--- js/leetCodePractice.js
+++ js/leetCodePractice.js
@@ -8,12 +8,35 @@
 //
 // findMaxWealth([[1,5],[7,3],[3,5]])
 
-function zipperArray(arr){
-    const half = Math.ceil(arr.length/2);
-    const firstHalf = arr.splice(0,half);
-    const secondHalf = arr.splice(-half)
-    var c = firstHalf.map((e,i)=>[e,secondHalf[i]])
-    console.log(c.flat())
-}
+// function zipperArray(arr){
+//     const half = Math.ceil(arr.length/2);
+//     const firstHalf = arr.splice(0,half);
+//     const secondHalf = arr.splice(-half)
+//     var c = firstHalf.map((e,i)=>[e,secondHalf[i]])
+//     console.log(c.flat())
+// }
 
-zipperArray([1,1,2,2]);
+// zipperArray([1,1,2,2]);
+
+//defang an IP address
+// function defangIP(str){
+//     const a = str.split('.')
+//     console.log(a.join('[.]'))
+// }
+//
+// defangIP("255.100.50.0")
+// defangIP('1.1.1.1')
+
+function goodpair(arr){
+    let count = 0
+    for(let i=0; i<arr.length;i++){
+        for(let j=i+1; j<arr.length; j++){
+            if(arr[i]==arr[j]){
+                count+=1
+            }
+        }
+    }
+    console.log(count)
+
+}
+goodpair([1,1,1,1])
