6d6d2eaf60cb6b6d5415996f1d60d87ac410f5b7
Kole Morgan <kmorgan0057@gmail.com>
2020-12-13 17:31:46 -0600

    Leetcode JS Practice

--- js/leetCodePractice.js
+++ js/leetCodePractice.js
@@ -1,9 +1,19 @@
-function findMaxWealth(arr){
-    var newArr=[];
-    for(let i=0; i<arr.length; i++){
-        newArr.push(arr[i].reduce((a,b)=> a+b,0))
-    }
-    console.log(Math.max(...newArr));
+// function findMaxWealth(arr){
+//     var newArr=[];
+//     for(let i=0; i<arr.length; i++){
+//         newArr.push(arr[i].reduce((a,b)=> a+b,0))
+//     }
+//     console.log(Math.max(...newArr));
+// }
+//
+// findMaxWealth([[1,5],[7,3],[3,5]])
+
+function zipperArray(arr){
+    const half = Math.ceil(arr.length/2);
+    const firstHalf = arr.splice(0,half);
+    const secondHalf = arr.splice(-half)
+    var c = firstHalf.map((e,i)=>[e,secondHalf[i]])
+    console.log(c.flat())
 }
 
-findMaxWealth([[1,5],[7,3],[3,5]])
+zipperArray([1,1,2,2]);
