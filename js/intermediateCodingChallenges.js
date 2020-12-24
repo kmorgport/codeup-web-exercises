91fbe2bab803ce3f3175214ff7ed16c859a48589
Kole Morgan <kmorgan0057@gmail.com>
2020-12-24 17:41:02 -0600

    Hash Table Notes

--- js/intermediateCodingChallenges.js
+++ js/intermediateCodingChallenges.js
@@ -2,11 +2,18 @@ function targetElements(arr,n){
     let storageHash = {}
     let nums = []
     for(let i in arr){
+        //for every iteration subtracts value from integer to create 'addend'
         let addend = n-arr[i]
+        //creates key-value pair in hashtable object
+        storageHash[arr[i]] = i
+        //so I can visually see my hash table
+        console.log(storageHash)
+        //if your addend value exists as a key in your hashtable
         if(addend in storageHash){
+            //push an array that is your addEnd plus whichever iteration value is creating a valid addend in your hashtable
             nums.push([addend, arr[i]])
         }
-        storageHash[arr[i]] = i
+        // storageHash[arr[i]] = i
     }
 
     return nums
