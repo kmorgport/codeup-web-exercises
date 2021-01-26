0c9117663a0c7eb9e97dad5459f6dfcf7737cc17
Kole Morgan <kmorgan0057@gmail.com>
2021-01-25 22:49:15 -0600

    Algorithms Exercises

--- js/dataStructures.js
+++ js/dataStructures.js
@@ -9,8 +9,24 @@ let anagram2 = {};
 for (let val of str1){
     anagram1[val] = (anagram1[val] || 0) + 1;
 }
-for(let val of str2){
+for(let val of str2) {
     anagram2[val] = (anagram2[val] || 0) + 1;
 }
-for
+    // console.log(anagram1)
+    // console.log(anagram2)
+for(let key in anagram1){
+    if(!(key in anagram2)){
+        return false;
+    }
+    if(anagram2[key]!== anagram1[key]){
+        return false;
+        }
+    }
+return true;
+
 }
+
+console.log(anagram("aaz", "zza"));
+console.log(anagram("anagram", "nagaram"));
+console.log(anagram("awesome","awesom"));
+console.log(anagram("rat","tar"))
