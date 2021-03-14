801ea9dd9ad03dd8ad7b5ade0394e0537bc66e37
Kole Morgan <kmorgan0057@gmail.com>
2021-03-14 18:14:57 -0500

    hackerrank practice

--- js/hackerrank.js
+++ js/hackerrank.js
@@ -121,18 +121,17 @@ function countApplesAndOranges(s,t,a,b,apples,oranges){
     let appInRange = 0;
     let ornInRange = 0;
     for(let i = 0; i<apples.length; i++){
-        if(((apples[i]+a)>=s)){
+        if(((apples[i]+a)>=s&&(apples[i]+a)<=t)){
             appInRange+=1;
         }
     }
     console.log(appInRange)
     for(let i = 0; i<oranges.length;i++){
-        // console.log(oranges[i]+a)
-        if((oranges[i]+b)<=t){
+        if((((oranges[i]+b)>=s)&&(oranges[i]+b)<=t)){
             ornInRange+=1
         }
     }
     console.log(ornInRange)
 }
 
-countApplesAndOranges(7,10,4,12,[2,3,-4],[3,-2,-4])
+countApplesAndOranges(7,11,5,15,[-2,2,1],[5,-6])
